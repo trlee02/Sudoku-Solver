bfe59131cf7220eb8d95fbed382443ca2df2d7d5
TristanLee <tristan.rene.lee@gmail.com>
2020-03-31 10:56:03 -0700

    Fixed checkBox

--- solver.py
+++ solver.py
@@ -10,76 +10,74 @@ board = [
     [0, 0, 0, 0, 8, 0, 0, 7, 9]
 ]
 
+# board = [
+#     [5, 3, 1, 1, 7, 1, 1, 1, 1],
+#     [6, 1, 1, 1, 9, 5, 1, 1, 1],
+#     [1, 9, 8, 1, 1, 1, 1, 6, 1],
+#     [8, 1, 1, 1, 6, 1, 1, 1, 3],
+#     [4, 1, 1, 8, 1, 3, 1, 1, 1],
+#     [7, 1, 1, 1, 2, 1, 1, 1, 6],
+#     [1, 6, 1, 1, 1, 1, 2, 8, 1],
+#     [1, 1, 1, 4, 1, 9, 1, 1, 5],
+#     [1, 1, 1, 1, 8, 1, 1, 7, 9]
+# ]
+
 
 def printBoard(board):
     for i in board:
         print(i)
-
-
-'''
-    find an open slot
-    if all slots are full
-        return true                 base condition
-
-    try to fill slot with a number
-    if it is filled
-        solve(board)                recursion condition
-    else
-        return false
-
-    '''
+    print()
 
 
 def findOpen(board):
     for i in range(len(board)):
-        if 0 in board[i]:
-            x = i
-            y = board[i].index(0)
-            return x, y
-        else:
-            return -1
+        for j in range(len(board[i])):
+            if board[i][j] == 0:
+                return (i, j)
+    return False
 
 
 def checkBox(board, x, y, num):
     xMin = xMax = yMin = yMax = 0
-    if x < 2:
+    if x < 3:
         xMin = 0
         xMax = 3
-    elif 2 < x < 6:
+    elif 3 <= x < 6:
         xMin = 3
         xMax = 6
     elif x > 6:
         xMin = 6
         xMax = 9
 
-    if y < 2:
+    if y < 3:
         yMin = 0
         yMax = 3
-    elif 2 < y < 6:
+    elif 3 <= y < 6:
         yMin = 3
         yMax = 6
     elif y > 6:
         yMin = 6
         yMax = 9
 
+    print(f"{xMin}, {xMax}, {yMin}, {yMax}")
     # checks left 3 big boxes
     for i in range(xMin, xMax):
         for j in range(yMin, yMax):
             if num == board[i][j]:
                 return False
-    print("works!")
     return True
 
 
 def check(board, x, y, num):
     if num in board[x]:
+        # print("hello")
         return False
 
     for i in range(len(board)):
         if board[i][y] == num:
             return False
 
-    if not checkBox(board, x, y, num):
+    if checkBox(board, x, y, num) == False:
         return False
 
     return True
@@ -88,18 +86,26 @@ def check(board, x, y, num):
 def solve(board):
     # creates tuple with x,y coordinates of empty space(0) in a list
     find = findOpen(board)
-    if find == -1:      # base statement
+    if find:
+        row, col = find
+    else:
         return True
 
-    for i in range(1, 9):
+    for i in range(1, 10):
         # checks to see what number works, then places it
-        if check(board, find[0], find[1], i):
-            board[find[0]][find[1]]
-        #
-        # then see if solve(board) is true
-        # Base statement
+        if check(board, row, col, i):
+            board[row][col] = i
+            printBoard(board)
+            break
+
+    # then see if solve(board) is true
+    # Base statement
+    if solve(board):
+        return True
+    board[row][col] = 0
+
+    return False
 
 
-printBoard(board)
-print(check(board, 1, 1, 8))
-# print(solve(board))
+# print(checkBox(board, 1, 2, 3))
+print(solve(board))
