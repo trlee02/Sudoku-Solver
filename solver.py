86ec580fc69de6ff0cab520f773f6b0ef5eb1522
TristanLee <tristan.rene.lee@gmail.com>
2020-03-31 11:46:03 -0700

    corrected recursion statement placement, works now

--- solver.py
+++ solver.py
@@ -1,13 +1,16 @@
+import os
+clear = lambda: os.system('cls')
+
 board = [
-    [5, 3, 0, 0, 7, 0, 0, 0, 0],
-    [6, 0, 0, 1, 9, 5, 0, 0, 0],
-    [0, 9, 8, 0, 0, 0, 0, 6, 0],
-    [8, 0, 0, 0, 6, 0, 0, 0, 3],
-    [4, 0, 0, 8, 0, 3, 0, 0, 1],
-    [7, 0, 0, 0, 2, 0, 0, 0, 6],
-    [0, 6, 0, 0, 0, 0, 2, 8, 0],
-    [0, 0, 0, 4, 1, 9, 0, 0, 5],
-    [0, 0, 0, 0, 8, 0, 0, 7, 9]
+            [0, 0, 0, 2, 6, 0, 7, 0, 1], 
+            [6, 8, 0, 0, 7, 0, 0, 9, 0], 
+            [1, 9, 0, 0, 0, 4, 5, 0, 0], 
+            [8, 2, 0, 1, 0, 0, 0, 4, 0], 
+            [0, 0, 4, 6, 0, 2, 9, 0, 0], 
+            [0, 5, 0, 0, 0, 3, 0, 2, 8], 
+            [0, 0, 9, 3, 0, 0, 0, 7, 4], 
+            [0, 4, 0, 0, 5, 0, 0, 3, 6], 
+            [7, 0, 3, 0, 1, 8, 0, 0, 0], 
 ]
 
 # board = [
@@ -59,7 +62,7 @@ def checkBox(board, x, y, num):
         yMin = 6
         yMax = 9
 
-    print(f"{xMin}, {xMax}, {yMin}, {yMax}")
+    #print(f"{xMin}, {xMax}, {yMin}, {yMax}")
     # checks left 3 big boxes
     for i in range(xMin, xMax):
         for j in range(yMin, yMax):
@@ -89,23 +92,27 @@ def solve(board):
     if find:
         row, col = find
     else:
+        printBoard(board)
         return True
 
     for i in range(1, 10):
         # checks to see what number works, then places it
         if check(board, row, col, i):
             board[row][col] = i
-            printBoard(board)
-            break
+            # clear()
+            # printBoard(board)
+            # Must be inside because as soon as a number is found move on to the next one
+            # If it was outside the if, and a number was not found
+            # It would leave it blank and then keep trying to check that number
+            if solve(board):
+                return True
 
-    # then see if solve(board) is true
-    # Base statement
-    if solve(board):
-        return True
-    board[row][col] = 0
+        # When no number is found to fill, backtrack
+        # 
+        board[row][col] = 0
 
+    # if no number was placed return False
     return False
 
 
-# print(checkBox(board, 1, 2, 3))
 print(solve(board))
